9450d8b7b68157f59e32cab86130b3774b6ea84b
keatnis <s17120026@itsm-tlapa.edu.mx>
2024-07-03 22:16:40 -0600

    modificacion del archivo main

--- src/main/java/com/keatnis/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/keatnis/LiterAlura/LiterAluraApplication.java
@@ -1,7 +1,8 @@
 package com.keatnis.LiterAlura;
 
 import com.keatnis.LiterAlura.main.Main;
-import com.keatnis.LiterAlura.service.LibroService;
+import com.keatnis.LiterAlura.repository.AutorRepository;
+import com.keatnis.LiterAlura.repository.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,8 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
     @Autowired
-    private LibroService libroService;
-
+    private LibroRepository libroRepository;
+    @Autowired
+    private AutorRepository autorRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(LiterAluraApplication.class, args);
@@ -19,7 +21,7 @@ public class LiterAluraApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Main main = new Main(libroService);
+        Main main = new Main(libroRepository,autorRepository);
         main.menu();
     }
 }
